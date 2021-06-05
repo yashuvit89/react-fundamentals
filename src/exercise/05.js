8be87ffc80ddb4756bd0ef7586dd7499a6300c5b
P S L Yaswanth <yashu.vit@gmail.com>
2021-06-05 23:12:40 +0530

    Add: All solutions to exercises in react-fundamentals

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -12,16 +12,28 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+
+function Box({className, style, children}) {
+  return React.createElement('div', {
+    className,
+    style,
+    fontStyle: 'italic',
+    children,
+  })
+}
 
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box className="box--medium" style={{backgroundColor: 'lightpink'}}>
+        medium pink box
+      </Box>
+      <Box className="box--large" style={{backgroundColor: 'orange'}}>
+        large orange box
+      </Box>
     </div>
   )
 }
--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,27 +4,38 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
+  const [userName, setUsername] = React.useState('')
+  // const inputRef = React.useRef(null)
+  // const [error, setError] = React.useState(null)
 
-  // 🐨 add the onSubmit handler to the <form> below
+  const handleSubmit = event => {
+    event.preventDefault()
+    onSubmitUsername()
+  }
 
-  // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
+  const handleInputChange = event => {
+    const value = event.target.value
+    setUsername(value.toLowerCase())
+    // setError(value !== value.toLowerCase())
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="userInput">Username:</label>
+        <input
+          // ref={inputRef}
+          id="userInput"
+          type="text"
+          value={userName}
+          onChange={handleInputChange}
+        />
       </div>
+      {/* {error && (
+        <div role="alert" style={{color: 'red'}}>
+          User must enter lower case
+        </div>
+      )} */}
       <button type="submit">Submit</button>
     </form>
   )
